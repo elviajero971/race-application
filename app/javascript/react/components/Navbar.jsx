32205392ba99b3a0a25ae75f5c9017bb49b80db5
lucasilliano <lucas.illiano@hotmail.com>
2025-02-10 11:01:18 +0100

    adjustments navbar elements

--- app/javascript/react/components/Navbar.jsx
+++ app/javascript/react/components/Navbar.jsx
@@ -2,26 +2,27 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-    const location = useLocation();
+    const { pathname } = useLocation();
+
+    const activeClasses = "px-4 py-2 bg-blue-500 text-white rounded transition-colors duration-200";
+    const inactiveClasses = "px-4 py-2 text-blue-500 hover:bg-blue-100 rounded transition-colors duration-200";
+
+    const isRaceActive = pathname.startsWith("/race") || pathname === "/";
+    const isUserActive = pathname.startsWith("/user");
 
     return (
         <nav className="my-5">
             <ul className="flex gap-4">
-                {location.pathname !== '/users' && (
-                    <li>
-                        <Link to="/users" className="text-blue-500 hover:underline">
-                            List of users
-                        </Link>
-                    </li>
-                )}
-
-                {location.pathname !== '/' && (
-                    <li>
-                        <Link to="/" className="text-blue-500 hover:underline">
-                            List of races
-                        </Link>
-                    </li>
-                )}
+                <li>
+                    <Link to="/" className={isRaceActive ? activeClasses : inactiveClasses}>
+                        List of races
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/users" className={isUserActive ? activeClasses : inactiveClasses}>
+                        List of users
+                    </Link>
+                </li>
             </ul>
         </nav>
     );
