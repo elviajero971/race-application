0f769160f72782db8ef1b14c75797c9852f997fb
lucasilliano <lucas.illiano@hotmail.com>
2025-02-10 12:44:35 +0100

    adding api routes comments to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,15 +3,19 @@ module Api
     class UsersController < ApplicationController
       skip_before_action :verify_authenticity_token
       before_action :set_user, only: [:show, :update, :destroy]
+
+      # GET /api/v1/users
       def index
         @users = User.all
         render json: @users, status: :ok
       end
 
+      # GET /api/v1/users/:id
       def show
         render json: @user, status: :ok
       end
 
+      # POST /api/v1/users
       def create
         user = User.new(user_params)
         if user.save
@@ -21,6 +25,7 @@ module Api
         end
       end
 
+      # PUT/PATCH /api/v1/users/:id
       def update
         if @user.update(user_params)
           render json: @user, status: :ok
@@ -29,6 +34,7 @@ module Api
         end
       end
 
+      # DELETE /api/v1/users/:id
       def destroy
         if @user.destroy
           render json: { message: "User deleted successfully" }, status: :ok
