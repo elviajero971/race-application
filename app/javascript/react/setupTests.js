518968da51525a7f86694576f81243105b2e4b6d
lucasilliano <lucas.illiano@hotmail.com>
2025-02-20 11:10:47 +0100

    adding package to test pages and use mocks

--- app/javascript/react/setupTests.js
+++ app/javascript/react/setupTests.js
@@ -1,5 +1,7 @@
 import '@testing-library/jest-dom';
 import { TextEncoder, TextDecoder } from 'util';
+import fetchMock from 'jest-fetch-mock';
+fetchMock.enableMocks();
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
